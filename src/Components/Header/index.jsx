a16d9a5939e44111fa9c2fd07b5248191cfa1f77
MageLink <mage.link.zhezard@gmail.com>
2022-09-29 14:19:49 -0300

    Header completed

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div className="header-container">Header o k ase</div>;
+  return (
+    <div className="header-container">
+      <span>
+        <Link to={"/"}>Home</Link> | <Link to="/favorites">Favoritos</Link>
+      </span>
+    </div>
+  );
 };
 
 export default Header;
