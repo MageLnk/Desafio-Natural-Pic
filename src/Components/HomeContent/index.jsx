916988f4032a02591a85309f064d5940cea23e47
MageLink <mage.link.zhezard@gmail.com>
2022-09-29 14:51:46 -0300

    Header title

--- src/Components/HomeContent/index.jsx
+++ src/Components/HomeContent/index.jsx
@@ -1,5 +1,12 @@
 const HomeContent = () => {
-  return <div>Home o k ase</div>;
+  return (
+    <div className="home-content-container">
+      <h1 className="home-content-title">Natual Pic</h1>
+      <div className="card-content-container">
+        <span>Cartas</span>
+      </div>
+    </div>
+  );
 };
 
 export default HomeContent;
